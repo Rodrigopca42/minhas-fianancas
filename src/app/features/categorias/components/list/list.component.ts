bc568981c7972649a3159bc1c947bf0e14bd2a76
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-08-11 17:31:03 -0300

    criação de categoriaService e criação do componente lista

--- src/app/features/categorias/components/list/list.component.ts
+++ src/app/features/categorias/components/list/list.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CategoriaService } from '../../service/categoria.service';
+
+export interface Categoria {
+  nome: string;
+  descricao: string;
+  id: number;
+}
+
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements AfterViewInit, OnInit{
+
+  displayedColumns: string[] = ['nome', 'descricao'];
+  dataSource = new MatTableDataSource<Categoria>();
+  categorias: Categoria[] = [];
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  constructor(private categoriaService: CategoriaService){
+
+  }
+  ngOnInit(): void {
+    this.categoriaService.getCategorias()
+    .subscribe((categorias: Categoria[])=>{
+      this.categorias = categorias;
+      this.dataSource.data = this.categorias;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }

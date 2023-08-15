2c3d601cf4e4b1fbcef421a82b5b01c10a46aef1
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-08-15 17:14:14 -0300

    Criando componentes de listagens de entarda e saída

--- src/app/features/entradas/components/list/list.component.ts
+++ src/app/features/entradas/components/list/list.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Entrada } from 'src/app/features/models/entrada.model';
+import { EntradasService } from '../../service/entradas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +12,52 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
-  
+  displayedColumns: string[] = ['nome', 'pago', 'data', 'valor', 'tipo', 'editar', 'excluir'];
+  dataSource = new MatTableDataSource<Entrada>();
+  entradas: Entrada[] = [];
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  constructor(
+    private entradasService: EntradasService,
+    private router: Router
+    ){
+
+  }
+  ngOnInit(): void {
+    this.buscarEntradas();
+
+  }
+
+  buscarEntradas(){
+    this.entradasService.getEntradas()
+    .subscribe((entradas: Entrada[]) =>{
+      this.entradas = entradas;
+      this.dataSource.data = this.entradas;
+    })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  chamarEdicao(entrada: Entrada){
+    this.router.navigate(['entradas', 'editar', entrada.id]);
+  }
+
+  excluir(id:number){
+    // this.entradasService.excluirCategoria(id)
+    // .subscribe(resposta =>{
+    //   this.buscarCategorias();
+    // })
+  }
+
+  novaEntrada(){
+    this.router.navigate(['entradas', 'nova-entradas'])
+  }
 
 }
+
+
--- src/app/features/entradas/service/entradas.service.ts
+++ src/app/features/entradas/service/entradas.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpBaseService } from 'src/app/shared/base/http-base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntradasService {
+export class EntradasService extends HttpBaseService{
+  private endpoint = 'entradas';
 
-  constructor() { }
+  constructor( protected override readonly injector: Injector) {
+    super(injector);
+   }
+
+getEntradas(): Observable<any>{
+return this.httpGet(`${this.endpoint}`);
+}
+
 }

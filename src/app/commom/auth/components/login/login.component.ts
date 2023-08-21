4ff043d896f6c7ea028092035302b5e5abd90620
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-08-21 16:21:02 -0300

    HTML e CSS do componente login criado

--- src/app/commom/auth/components/login/login.component.ts
+++ src/app/commom/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  
 
 }
